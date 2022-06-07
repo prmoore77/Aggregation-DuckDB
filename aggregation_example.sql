8e6457c10f87c9ca4d5197e2553ee8e9c313df1e
Philip Moore <prmoore77@hotmail.com>
2022-06-07 13:52:51 -0400

    Added CSV results

--- aggregation_example.sql
+++ aggregation_example.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE TABLE product_nodes
 (
-    node_id                VARCHAR(36) DEFAULT uuid()
+    node_id                VARCHAR(36)  DEFAULT uuid() -- Making this a VARCHAR instead of UUID type so that the JDBC driver can work
   , node_natural_key       INTEGER      NOT NULL
   , node_name              VARCHAR(100) NOT NULL
   , level_name             VARCHAR(100) NOT NULL
@@ -55,6 +55,39 @@ VALUES (202, 'Nerds', 'UPC', (SELECT node_id
 SELECT *
   FROM product_nodes AS t;
 
+-- Create a temporary table that derives root/leaf attributes from the nodes
+CREATE OR REPLACE TEMPORARY TABLE product_nodes_temp
+AS
+SELECT node_id
+       , node_natural_key
+       , node_name
+       , level_name
+       , parent_node_id
+       , is_root
+       , is_leaf
+   FROM (SELECT node_id
+              , node_natural_key
+              , node_name
+              , level_name
+              , parent_node_id
+              , CASE WHEN parent_node_id IS NULL
+                        THEN TRUE
+                        ELSE FALSE
+                END AS is_root
+              , CASE WHEN node_id IN (SELECT parent_node_id
+                                        FROM product_nodes
+                                     )
+                        THEN FALSE
+                        ELSE TRUE
+                END AS is_leaf
+           FROM product_nodes
+        );
+
+-- Inspect the data
+SELECT *
+  FROM product_nodes_temp AS t;
+
+
 -- Recursively Build a Dimension structure from the data for reporting...
 CREATE OR REPLACE TABLE product_reporting_dim
 AS
@@ -64,6 +97,8 @@ WITH RECURSIVE parent_nodes (
   , node_name
   , level_name
   , parent_node_id
+  , is_root
+  , is_leaf
   , level_number
   , node_json
   , node_json_path
@@ -76,12 +111,16 @@ AS (
       , node_name
       , level_name
       , parent_node_id
+      , is_root
+      , is_leaf
       , 1 AS level_number
       , {node_id: node_id,
          node_natural_key: node_natural_key,
          node_name: node_name,
          level_name: level_name,
          parent_node_id: parent_node_id,
+         is_root: is_root,
+         is_leaf: is_leaf,
          level_number: 1
         } AS node_json
       , [{node_id: node_id,
@@ -89,9 +128,11 @@ AS (
          node_name: node_name,
          level_name: level_name,
          parent_node_id: parent_node_id,
+         is_root: is_root,
+         is_leaf: is_leaf,
          level_number: 1
         }] AS node_json_path
-       FROM product_nodes
+       FROM product_nodes_temp
       WHERE parent_node_id IS NULL
     UNION ALL
     -- Recursive Clause
@@ -101,24 +142,30 @@ AS (
       , nodes.node_name
       , nodes.level_name
       , nodes.parent_node_id
+      , nodes.is_root
+      , nodes.is_leaf
       , parent_nodes.level_number + 1 AS level_number
       , {node_id: nodes.node_id,
          node_natural_key: nodes.node_natural_key,
          node_name: nodes.node_name,
          level_name: nodes.level_name,
          parent_node_id: nodes.parent_node_id,
+         is_root: nodes.is_root,
+         is_leaf: nodes.is_leaf,
          level_number: parent_nodes.level_number + 1
         } AS node_json
        , array_append (parent_nodes.node_json_path
-      , {node_id: nodes.node_id
-      , node_natural_key: nodes.node_natural_key
-      , node_name: nodes.node_name
-      , level_name: nodes.level_name
-      , parent_node_id: nodes.parent_node_id
-      , level_number: parent_nodes.level_number + 1
-        }
+                     , {node_id: nodes.node_id,
+                        node_natural_key: nodes.node_natural_key,
+                        node_name: nodes.node_name,
+                        level_name: nodes.level_name,
+                        parent_node_id: nodes.parent_node_id,
+                        is_root: nodes.is_root,
+                        is_leaf: nodes.is_leaf,
+                        level_number: parent_nodes.level_number + 1
+                       }
         ) AS node_json_path
-       FROM product_nodes AS nodes
+       FROM product_nodes_temp AS nodes
           JOIN
             parent_nodes
           ON nodes.parent_node_id = parent_nodes.node_id
@@ -128,12 +175,20 @@ SELECT node_id
      , node_name
      , level_name
      , parent_node_id
+     , is_root
+     , is_leaf
      , level_number
-     --
-     , CASE WHEN level_number >= COALESCE (LEAD (level_number) OVER (ORDER BY REPLACE (node_json_path::VARCHAR, ']', '') ASC NULLS LAST), level_number)
-               THEN TRUE
-               ELSE FALSE
-       END AS is_leaf
+     -- Construct a new node_json struct value to include the sort order...
+     , {node_id: node_id,
+        node_natural_key: node_natural_key,
+        node_name: node_name,
+        level_name: level_name,
+        parent_node_id: parent_node_id,
+        is_root: is_root,
+        is_leaf: is_leaf,
+        level_number: level_number,
+        node_sort_order: ROW_NUMBER () OVER (ORDER BY REPLACE (node_json_path::VARCHAR, ']', '') ASC NULLS LAST)} AS node_json
+     , node_json_path
      , ROW_NUMBER () OVER (ORDER BY REPLACE (node_json_path::VARCHAR, ']', '') ASC NULLS LAST) AS node_sort_order
      -- Level 1 columns
      , node_json_path[1].node_id            AS level_1_node_id
@@ -153,7 +208,7 @@ SELECT node_id
   FROM parent_nodes
 ORDER BY node_sort_order ASC;
 
-SELECT *
+SELECT * EXCLUDE (node_id, parent_node_id, level_1_node_id, level_2_node_id, level_3_node_id, node_json, node_json_path)
   FROM product_reporting_dim
 ORDER BY node_sort_order ASC;
 
@@ -167,8 +222,9 @@ WITH RECURSIVE parent_nodes (
   , node_name
   , level_name
   , parent_node_id
-  , level_number
+  , is_root
   , is_leaf
+  , level_number
   , node_sort_order
   , node_json
   , node_json_path
@@ -181,27 +237,13 @@ AS (
       , node_name
       , level_name
       , parent_node_id
-      , level_number
+      , is_root
       , is_leaf
+      , level_number
       , node_sort_order
-      , {node_id: node_id,
-         node_natural_key: node_natural_key,
-         node_name: node_name,
-         level_name: level_name,
-         parent_node_id: parent_node_id,
-         level_number: level_number,
-         is_leaf: is_leaf,
-         node_sort_order: node_sort_order
-        } AS node_json
-      , [{node_id: node_id,
-         node_natural_key: node_natural_key,
-         node_name: node_name,
-         level_name: level_name,
-         parent_node_id: parent_node_id,
-         level_number: level_number,
-         is_leaf: is_leaf,
-         node_sort_order: node_sort_order
-        }] AS node_json_path
+      , node_json
+      -- We must start a new NODE_JSON array b/c each node will be represented as a root node...
+      , [node_json] AS node_json_path
        FROM product_reporting_dim
        -- We do NOT filter the anchor, because we want EVERY node in the hierarchy to be a root node...
     UNION ALL
@@ -212,48 +254,35 @@ AS (
       , nodes.node_name
       , nodes.level_name
       , nodes.parent_node_id
-      , nodes.level_number
+      , nodes.is_root
       , nodes.is_leaf
+      , nodes.level_number
       , nodes.node_sort_order
-      , {node_id: nodes.node_id,
-         node_natural_key: nodes.node_natural_key,
-         node_name: nodes.node_name,
-         level_name: nodes.level_name,
-         parent_node_id: nodes.parent_node_id,
-         level_number: nodes.level_number,
-         is_leaf: nodes.is_leaf,
-         node_sort_order: nodes.node_sort_order
-        } AS node_json
-       , array_append (parent_nodes.node_json_path
-      , {node_id: nodes.node_id
-      , node_natural_key: nodes.node_natural_key
-      , node_name: nodes.node_name
-      , level_name: nodes.level_name
-      , parent_node_id: nodes.parent_node_id
-      , level_number: nodes.level_number
-      , is_leaf: nodes.is_leaf
-      , node_sort_order: nodes.node_sort_order
-        }
-        ) AS node_json_path
+      , nodes.node_json
+      , array_append (parent_nodes.node_json_path
+                    , nodes.node_json
+                     ) AS node_json_path
        FROM product_reporting_dim AS nodes
           JOIN
             parent_nodes
           ON nodes.parent_node_id = parent_nodes.node_id
 )
-SELECT -- Ancestor columns
+SELECT -- Ancestor columns (we take the first array element to get the anchor root)
         node_json_path[1].node_id             AS ancestor_node_id
       , node_json_path[1].node_natural_key    AS ancestor_node_natural_key
       , node_json_path[1].node_name           AS ancestor_node_name
       , node_json_path[1].level_name          AS ancestor_level_name
       , node_json_path[1].level_number        AS ancestor_level_number
+      , node_json_path[1].is_root             AS ancestor_is_root
       , node_json_path[1].is_leaf             AS ancestor_is_leaf
       , node_json_path[1].node_sort_order     AS ancestor_node_sort_order
-      --
+      -- Descendant columns
       , node_id                               AS descendant_node_id
       , node_natural_key                      AS descendant_node_natural_key
       , node_name                             AS descendant_node_name
       , level_name                            AS descendant_level_name
       , level_number                          AS descendant_level_number
+      , is_root                               AS descendant_is_root
       , is_leaf                               AS descendant_is_leaf
       , node_sort_order                       AS descendant_node_sort_order
       --
@@ -347,7 +376,17 @@ VALUES ((SELECT node_natural_key
       , 0.50
        );
 
--- Now perform hierarchical aggregations
+INSERT INTO sales_facts (product_id, customer_id, date_id, unit_quantity, sales_amount)
+VALUES ((SELECT node_natural_key
+           FROM product_nodes
+          WHERE node_name = 'Spinach')
+      , 'Phil'
+      , DATE '2022-01-05'
+      , 2
+      , 2.00
+       );
+
+-- Now perform easy hierarchical aggregations with the exploded hierarchy table
 SELECT LPAD (' ', (products.ancestor_level_number - 1) * 7, ' ')
      || products.ancestor_level_name       AS product_level_name
      , LPAD (' ', (products.ancestor_level_number - 1) * 7, ' ')
@@ -361,12 +400,48 @@ SELECT LPAD (' ', (products.ancestor_level_number - 1) * 7, ' ')
     JOIN
        product_aggregation_dim AS products
     ON facts.product_id = products.descendant_node_natural_key
-GROUP BY products.ancestor_node_id
-       , products.ancestor_node_natural_key
-       , products.ancestor_node_name
+GROUP BY products.ancestor_node_name
        , products.ancestor_level_name
        , products.ancestor_level_number
-       , products.ancestor_is_leaf
        , products.ancestor_node_sort_order
 ORDER BY products.ancestor_node_sort_order ASC
+;
+
+-- You can also aggregate with the flattened reporting dimension table, but the SQL is much more complicated
+-- AND it must change depending on how many levels you are aggregating...
+SELECT CASE WHEN GROUPING (level_3_node_name) = 0
+               THEN '              ' || products.level_3_level_name
+            WHEN GROUPING (level_2_node_name) = 0
+               THEN '       ' || products.level_2_level_name
+            WHEN GROUPING (level_1_node_name) = 0
+               THEN products.level_1_level_name
+       END AS product_level_name
+     , CASE WHEN GROUPING (level_3_node_name) = 0
+               THEN '              ' || products.level_3_node_name
+            WHEN GROUPING (level_2_node_name) = 0
+               THEN '       ' || products.level_2_node_name
+            WHEN GROUPING (level_1_node_name) = 0
+               THEN products.level_1_node_name
+       END AS product_node_name
+     -- Aggregates
+     , SUM (facts.sales_amount)           AS sum_sales_amount
+     , SUM (facts.unit_quantity)          AS sum_unit_quantity
+     , COUNT (DISTINCT facts.customer_id) AS distinct_customer_count
+     , COUNT (*)                          AS count_of_fact_records
+  FROM sales_facts AS facts
+    JOIN
+       product_reporting_dim AS products
+    ON facts.product_id = products.node_natural_key
+GROUP BY ROLLUP ((products.level_1_level_name
+                , products.level_1_node_name
+                 )
+               , (products.level_2_level_name
+                , products.level_2_node_name
+                 )
+               , (products.level_3_level_name
+                , products.level_3_node_name
+                 )
+                )
+-- Throw out the "GRAND TOTAL" grouping set...
+HAVING NOT GROUPING (products.level_1_node_name) = 1
 ;
